9579be00b267ba3561b88e5bb462d3384993a4bd
Tony Borchert <65500763+bit-burger@users.noreply.github.com>
2025-09-13 19:36:24 +0200

    improve test suite

---
+++ manual_test/color_test.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dart_tui/ansi/ansi_terminal_window.dart';
+import 'package:dart_tui/core/style.dart';
+import 'package:dart_tui/core/terminal.dart';
+
+class ColorListener extends DefaultTerminalListener {
+  @override
+  void controlCharacter(ControlCharacter c) async {
+    if (c == ControlCharacter.ctrlZ) {
+      await window.destroy();
+      exit(0);
+    }
+  }
+
+  @override
+  void screenResize(Size size) {
+    paint();
+  }
+}
+
+final window = AnsiTerminalWindow.agnostic(listener: ColorListener());
+int plus = 0;
+
+void paint() {
+  window.screen.optimizeForFullDraw();
+  for (int j = 0; j < window.size.height; j++) {
+    for (int i = 0; i < window.size.width; i++) {
+      final color = XTermTerminalColor(color: (plus + i + j) % 256);
+      window.drawPoint(position: Position(i, j), background: color);
+    }
+  }
+  window.updateScreen();
+}
+
+void main() async {
+  await window.attach();
+  window.controller.changeCursorVisibility(hiding: true);
+  paint();
+  Timer.periodic(Duration(milliseconds: 1000 ~/ 60), (_) {
+    plus += 2;
+    paint();
+  });
+}
---
+++ manual_test/listen_test.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:dart_tui/ansi/ansi_terminal_window.dart';
+import 'package:dart_tui/core/terminal.dart';
+
+
+
+class ControlTerminalInputListener implements TerminalListener {
+  @override
+  void controlCharacter(ControlCharacter controlCharacter) async {
+    stdout.write("$controlCharacter;");
+    if (controlCharacter == ControlCharacter.ctrlZ) {
+      await window.destroy();
+      exit(0);
+    }
+  }
+
+  @override
+  void input(String s) {
+    stdout.write("input('$s')");
+  }
+
+  @override
+  void screenResize(Size size) {
+    stdout.write("resize(width:${size.width},height:${size.height});");
+  }
+
+  @override
+  void signal(AllowedSignal signal) {
+    stdout.write("signal($signal);");
+  }
+
+  @override
+  void focusChange(bool isFocused) {
+    stdout.write("focuschange($isFocused);");
+  }
+
+  @override
+  void mouseEvent(MouseEvent event) {
+    switch (event) {
+      case MouseButtonPressEvent(pressType: var t, button: var b):
+        stdout.write("press($t,$b)");
+      case MouseScrollEvent(xScroll: var x, yScroll: var y):
+        stdout.write("scroll(x:$x,y:$y);");
+      case MouseHoverMotionEvent(position: var pos):
+        stdout.write("motion(x:${pos.x},y:${pos.y})");
+    }
+  }
+}
+
+final window = AnsiTerminalWindow.agnostic(listener: ControlTerminalInputListener());
+
+void main() async {
+  await window.attach();
+  stdout.write("start;");
+  window.screen.optimizeForFullDraw();
+}
