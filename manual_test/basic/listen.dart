6b2ef60b80ab7ce2d1183240386c71b9a4b97c25
Tony Borchert <65500763+bit-burger@users.noreply.github.com>
2025-09-21 01:38:43 +0200

    improve listen test

--- manual_test/basic/listen.dart
+++ manual_test/basic/listen.dart
@@ -3,12 +3,22 @@ import 'dart:io';
 import 'package:dart_tui/ansi/ansi_terminal_window.dart';
 import 'package:dart_tui/core/terminal.dart';
 
+List<String> buff = List.filled(1000, "", growable: true);
 
+void _print(String nexText) {
+  buff.add(nexText);
+  window.drawBackground();
+  for(int i = 0; i < window.size.height; i++) {
+    final text = buff[buff.length - i - 1];
+    window.drawText(text: text, position: Position(0, i));
+  }
+  window.updateScreen();
+}
 
 class ControlTerminalInputListener implements TerminalListener {
   @override
   void controlCharacter(ControlCharacter controlCharacter) async {
-    stdout.write("$controlCharacter;");
+    _print("$controlCharacter;");
     if (controlCharacter == ControlCharacter.ctrlZ) {
       await window.destroy();
       exit(0);
@@ -17,40 +27,51 @@ class ControlTerminalInputListener implements TerminalListener {
 
   @override
   void input(String s) {
-    stdout.write("input('$s')");
+    _print("input('$s')");
   }
 
   @override
   void screenResize(Size size) {
-    stdout.write("resize(width:${size.width},height:${size.height});");
+    _print("resize(width:${size.width},height:${size.height});");
   }
 
   @override
   void signal(AllowedSignal signal) {
-    stdout.write("signal($signal);");
+    _print("signal($signal);");
   }
 
   @override
   void focusChange(bool isFocused) {
-    stdout.write("focuschange($isFocused);");
+    _print("focuschange($isFocused);");
   }
 
   @override
   void mouseEvent(MouseEvent event) {
     switch (event) {
-      case MouseButtonPressEvent(pressType: var t, button: var b):
-        stdout.write("press($t,$b)");
-      case MouseScrollEvent(xScroll: var x, yScroll: var y):
-        stdout.write("scroll(x:$x,y:$y);");
+      case MouseButtonPressEvent(
+        pressType: var t,
+        button: var b,
+        position: var pos,
+      ):
+        final bs = b.toString().substring(12);
+        if (t == MouseButtonPressEventType.press) {
+          _print("press($bs,x:${pos.x},y:${pos.y})");
+        } else {
+          _print("release($bs,x:${pos.x},y:${pos.y})");
+        }
+      case MouseScrollEvent(xScroll: var x, yScroll: var y, position: var pos):
+        _print("scroll(scrollX:$x,scrollY:$y,x:${pos.x},y:${pos.y});");
       case MouseHoverMotionEvent(position: var pos):
-        stdout.write("motion(x:${pos.x},y:${pos.y})");
+        _print("motion(x:${pos.x},y:${pos.y})");
     }
   }
 }
 
-final window = AnsiTerminalWindow.agnostic(listener: ControlTerminalInputListener());
+final window = AnsiTerminalWindow.agnostic(
+  listener: ControlTerminalInputListener(),
+);
 
 void main() async {
   await window.attach();
-  stdout.write("start;");
+  _print("start;");
 }
